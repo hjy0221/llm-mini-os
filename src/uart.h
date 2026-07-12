5af42e17b383a5d4fb4709f8bf4e4aa56828aaf0
Ha Jae-Yoon <ds970221@gmail.com>
2026-07-12 13:11:56 +0900

    Add uart_put_hex64 function declaration

--- src/uart.h
+++ src/uart.h
@@ -1,8 +1,11 @@
 #ifndef UART_H
 #define UART_H
 
+#include <stdint.h>
+
 void uart_putc(char c);
 void uart_puts(const char *text);
+void uart_put_hex64(uint64_t value);
 char uart_getc(void);
 
 #endif
