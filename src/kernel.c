1a12adc3611a46052b61f816dd789db24d1c010f
Ha Jae-Yoon <ds970221@gmail.com>
2026-07-12 13:11:38 +0900

    Add exception initialization in kernel_main
    Initialize exception handling in kernel main function.

--- src/kernel.c
+++ src/kernel.c
@@ -1,7 +1,10 @@
+#include "exception.h"
 #include "shell.h"
 #include "uart.h"
 
 void kernel_main(void) {
+    exception_init();
+
     uart_puts("\nHello from ARM64 QEMU Mini OS!\n");
     uart_puts("Kernel is running on QEMU virt.\n");
     uart_puts("Type 'help' to see available commands.\n\n");
