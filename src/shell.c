44a002e68b280821f79b776c2a41af3cf73aabd8
Ha Jae-Yoon <ds970221@gmail.com>
2026-07-19 06:49:57 +0900

    Add ticks and uptime shell commands

--- src/shell.c
+++ src/shell.c
@@ -3,6 +3,7 @@
 #include "console.h"
 #include "exception.h"
 #include "platform.h"
+#include "timer.h"
 #include "uart.h"
 
 #include <stddef.h>
@@ -26,6 +27,8 @@ static void command_help(void) {
     uart_puts("  hello   Print a greeting\n");
     uart_puts("  clear   Clear the terminal\n");
     uart_puts("  info    Show OS information\n");
+    uart_puts("  ticks   Show timer tick count\n");
+    uart_puts("  uptime  Show seconds since boot\n");
     uart_puts("  fault   Trigger a test exception and halt\n");
     uart_puts("  reboot  Restart the virtual machine\n");
 }
@@ -37,6 +40,24 @@ static void command_info(void) {
     uart_puts("  Console:      PL011 UART\n");
     uart_puts("  Shell:        built-in\n");
     uart_puts("  Exceptions:   VBAR_EL1 installed\n");
+    uart_puts("  Interrupts:   GICv2\n");
+    uart_puts("  Timer:        ARM Generic Timer, ");
+    uart_put_uint64(timer_frequency());
+    uart_puts(" Hz, ");
+    uart_put_uint64(TIMER_TICKS_PER_SECOND);
+    uart_puts(" ticks/s\n");
+}
+
+static void command_ticks(void) {
+    uart_puts("Timer ticks: ");
+    uart_put_uint64(timer_ticks());
+    uart_puts("\n");
+}
+
+static void command_uptime(void) {
+    uart_puts("Uptime: ");
+    uart_put_uint64(timer_uptime_seconds());
+    uart_puts(" seconds\n");
 }
 
 static void execute_command(const char *command) {
@@ -48,6 +69,10 @@ static void execute_command(const char *command) {
         uart_puts("\x1b[2J\x1b[H");
     } else if (strings_equal(command, "info")) {
         command_info();
+    } else if (strings_equal(command, "ticks")) {
+        command_ticks();
+    } else if (strings_equal(command, "uptime")) {
+        command_uptime();
     } else if (strings_equal(command, "fault")) {
         uart_puts("Triggering a BRK exception...\n");
         exception_trigger_test();
