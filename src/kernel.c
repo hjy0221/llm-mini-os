9413617b753e89f5e68218a9d867828c8190d935
Ha Jae-Yoon <ds970221@gmail.com>
2026-07-19 06:49:44 +0900

    Initialize GIC and generic timer

--- src/kernel.c
+++ src/kernel.c
@@ -1,9 +1,15 @@
 #include "exception.h"
+#include "gic.h"
+#include "irq.h"
 #include "shell.h"
+#include "timer.h"
 #include "uart.h"
 
 void kernel_main(void) {
     exception_init();
+    gic_init();
+    timer_init();
+    irq_enable();
 
     uart_puts("\nHello from ARM64 QEMU Mini OS!\n");
     uart_puts("Kernel is running on QEMU virt.\n");
